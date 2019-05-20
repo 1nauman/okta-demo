c64f922af9af7da078a293eccfb252149177fa3e
numan-r <nauman.rehman@gmail.com>
2019-05-20 13:24:25 +0530

    Added basewebviewpage

---
+++ OktaWebDemo/Views/BaseWebViewPage.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace OktaWebDemo.Views
+{
+    public abstract class BaseWebViewPage : WebViewPage
+    {
+        protected bool IsAuthenticated => Context.User.Identity.IsAuthenticated;
+        protected string IdentityName => Context.User.Identity.Name;
+        protected string IdentityEmail => ((ClaimsIdentity) Context.User.Identity).Claims
+                                          .SingleOrDefault(o => o.Type.Equals("email"))?.Value ?? string.Empty;
+    }
+
+    public abstract class BaseWebViewPage<TModel> : WebViewPage<TModel>
+    {
+        protected bool IsAuthenticated => Context.User.Identity.IsAuthenticated;
+        protected string IdentityName => Context.User.Identity.Name;
+        protected string IdentityEmail => ((ClaimsIdentity)Context.User.Identity).Claims
+                                          .SingleOrDefault(o => o.Type.Equals("email"))?.Value ?? string.Empty;
+    }
+}
